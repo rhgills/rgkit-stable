4d677528c23fa0367175b2ee5781136e44c77293
Robert Gilliam <robert@robertgilliam.org>
2013-06-18 13:11:07 -0700

    Add Tests.

---
+++ Tests/RGKitTests.h
@@ -0,0 +1,13 @@
+//
+//  RGKit_TestsTests.h
+//  RGKit TestsTests
+//
+//  Created by Robert Gilliam on 6/18/13.
+//  Copyright (c) 2013 Robert Gilliam. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RGKit_TestsTests : SenTestCase
+
+@end
