dea8a6fb685d7b9915562be0eb5b83e8e5a5aed6
Robert Gilliam <robert@robertgilliam.org>
2013-06-27 13:36:46 -0700

    add some missing headers

--- Classes/RGKit/RHGTime.h
+++ Classes/RGKit/RHGTime.h
@@ -11,6 +11,8 @@
 #import "Time/Clock/RHGClockImpl.h"
 #import "Time/Clock/RHGFakeFrozenClock.h"
 #import "Time/Clock/RHGFakeAutoadvancingClock.h"
+#import "Time/Clock/Timer Wrapper/RHGTimerWrapper.h"
+#import "Time/Clock/Timer Wrapper/RHGTimerWrapperDelegate.h"
 #import "Time/Clock/Timer Wrapper/RHGTimerWrapperNS.h"
 #import "Time/Clock/Block Call Scheduler/RHGBlockCallScheduler.h"
 #import "Time/Clock/Block Call Scheduler/RHGBlockCallSchedulerTimerImpl.h"
